9b47b5261ed57d611f9dcdad651918ae14996ad0
SergeyTigrov <info@websee.ru>
2013-05-30 17:18:09 +1100

    Add config

---
+++ main.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by WebSee.
+ * User: SergeyTigrov
+ * Date: 30.05.12
+ * Time: 12:15
+ */
+ 
+ $domain = 'domain.com';
+ 
+return array(
+    'components'=>array(
+        'user'=>array(
+            'class' => 'WebUser',
+            // enable cookie-based authentication
+            'allowAutoLogin'=>true,
+            'autoRenewCookie'=>true,
+            'identityCookie' => array('domain' => '.'.$domain),
+        ),
+        'session' => array(
+            'class' => 'Session',
+            'connectionID' => 'db',
+            'cookieParams' => array('domain' => '.'.$domain),
+            'autoStart' => false,
+        ),
+    }
+}
